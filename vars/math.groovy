52ce7f318805aec5c33b7b3a7941834c91375aa2
Balamurali81 <70313343+Balamurali81@users.noreply.github.com>
2023-02-07 11:45:08 -0500

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,9 @@
+def add(x,y){
+    echo "Welcome to JSL scripting scripting of adding two numbers"
+  echo "Sum of ${x} and ${y} is ${x+y}"
+ }
+
+def sub(x,y){
+    echo "Welcome to JSL scripting scripting of substraction two numbers"
+  echo "Sum of ${x} and ${y} is ${x+y}"
+ }
